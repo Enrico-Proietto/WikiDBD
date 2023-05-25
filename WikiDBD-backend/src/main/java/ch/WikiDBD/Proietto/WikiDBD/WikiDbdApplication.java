b2c3717ccd75ee964308115e0a696865cd7ece38
Enrico Proietto <eproietto03@gmail.com>
2023-05-25 17:56:08 +0200

    initial commit

---
+++ WikiDBD-backend/src/main/java/ch/WikiDBD/Proietto/WikiDBD/WikiDbdApplication.java
@@ -0,0 +1,13 @@
+package ch.WikiDBD.Proietto.WikiDBD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiDbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiDbdApplication.class, args);
+	}
+
+}
